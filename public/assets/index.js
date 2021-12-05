34c2b30b9756d2ae41b3bdff155b0e27520bb37f
Julian <cusco2891@gmail.com>
2021-12-05 14:54:41 -0500

    front

--- public/assets/index.js
+++ public/assets/index.js
@@ -2,6 +2,6 @@ let top10;
 console.log("front-end");
 
 
-fetch('/top10').then(response => response.json()).then(data => {top10 = data});
+fetch('/top10').then(response => response.json()).then(data => top10 = data);
 
 console.log(top10);
