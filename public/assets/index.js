506b2484ba73ab0ddb4ad180f8e34af211ae0c19
Julian <cusco2891@gmail.com>
2021-12-06 19:36:49 -0500

    dbref check

--- public/assets/index.js
+++ public/assets/index.js
@@ -2,10 +2,10 @@ fetch('/7alltimetop10').then(response => response.json()).then(data => {
     console.log("7day top10 all time -->",data);
 });
 
-fetch('/7dailytop10').then(response => response.json()).then(data => {
-    console.log("7day top10 daily -->",data);
-});
+// fetch('/7dailytop10').then(response => response.json()).then(data => {
+//     console.log("7day top10 daily -->",data);
+// });
 
-fetch('/7monthlytop10').then(response => response.json()).then(data => {
-    console.log("7day top10 monthly -->",data);
-});
+// fetch('/7monthlytop10').then(response => response.json()).then(data => {
+//     console.log("7day top10 monthly -->",data);
+// });
--- server.js
+++ server.js
@@ -42,8 +42,8 @@ app.get("/", function (req, res) {
 
 // ---------------------------------------- FIREBASE TOP 10 SCORES ----------------------------------------------
 
-let before24Hour = new Date().getTime() - (24 * 3600 * 1000);
-let before30Day = new Date().getTime() - (24 * 30 * 3600 * 1000);
+// let before24Hour = new Date().getTime() - (24 * 3600 * 1000);
+// let before30Day = new Date().getTime() - (24 * 30 * 3600 * 1000);
 // ---------------------------- All Time Top10 ----------------------------->>
 
 app.get("/7alltimetop10", function (req, res) {
@@ -58,25 +58,25 @@ app.get("/7alltimetop10", function (req, res) {
 
 // ------------- 7/14/30 24-hour Top10 ------------------------------------->>
 
-app.get("/7dailytop10", function (req, res) {
+// app.get("/7dailytop10", function (req, res) {
 
-    database.ref().child('7dayscore').orderByChild('dateAdded').startAt(before24Hour).on('value', function (snap) {
-        return res.send(snapshot.val());
-    });
+//     database.ref().child('7dayscore').orderByChild('dateAdded').startAt(before24Hour).on('value', function (snap) {
+//         return res.send(snapshot.val());
+//     });
 
-});
+// });
 
 
 
-// --------------- 7/14/30 30-Day Top10 ------------------------------------>>
+// // --------------- 7/14/30 30-Day Top10 ------------------------------------>>
 
-app.get("/7monthlytop10", function (req, res) {
+// app.get("/7monthlytop10", function (req, res) {
 
-    database.ref().child('7dayscore').orderByChild('dateAdded').startAt(before30Day).on('value', function (snap) {
-        return res.send(snapshot.val());
-    });
+//     database.ref().child('7dayscore').orderByChild('dateAdded').startAt(before30Day).on('value', function (snap) {
+//         return res.send(snapshot.val());
+//     });
 
-});
+// });
 
 
 
